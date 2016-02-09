2ec6fcaaf326705b38ef3c0e17bdb0577095618e
sreekumar <sreekumarvpl@gmail.com>
2016-02-09 16:06:35 +0530

    static

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -126,15 +126,13 @@ ALLOWED_HOSTS = ['*']
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_dir')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
+
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR,'static/'),
-)
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
 )
 
 # Simplified static file serving.
