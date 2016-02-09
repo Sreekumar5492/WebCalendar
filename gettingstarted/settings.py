6bc03a9133ea6d7543530ad9450defb4c0511e6f
sreekumar <sreekumarvpl@gmail.com>
2016-02-09 15:45:55 +0530

    all

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'hello'
+    'hello',
+    'web_calendar',
+    'login',
 )
 
 MIDDLEWARE_CLASSES = (
