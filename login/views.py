62ceb9e478ee654d38ac3c8ab5d53407129477bf
sreekumar <sreekumarvpl@gmail.com>
2016-02-12 02:13:45 +0530

    validations added for login and signup

--- login/views.py
+++ login/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render ,redirect
 from django.contrib.auth import authenticate
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout
-from web_calendar.forms import userprofileform
+from web_calendar.models import *
 from django.contrib.auth.models import User
 from django.contrib import messages
 # Create your views here.
@@ -31,21 +31,28 @@ def signup(request):
         pwd2      = request.POST['password2']
         if pwd1 == pwd2:
             username    = request.POST['username']
-            user        = User.objects.create_user(username,password=pwd1)
-            form        = userprofileform(request.POST)
-            form.user   = user
-            print form
-            if form.is_valid():
+            name        = request.POST['name']
+            email       = request.POST['email']
+            try:
+                user        = User.objects.create_user(username,password=pwd1)
                 user.save()
-                userprofile = form.save(commit=False)
-                userprofile.user = user
-                userprofile.save()
+            except:
+                 messages.add_message(request, messages.ERROR, "Username already exists!")
+                 args = {'name':name,
+                         'email':email
+                        }
+                 return render(request,'login/signup.html',args)
+            
+            try:
+                userp       = userprofile()
+                userp.name  = name
+                userp.email = email
+                userp.user  = user
+                userp.save()
                 return render(request,'login/login.html')
-            else:
+            except Exception,e:
+                messages.add_message(request, messages.ERROR, "Failed! Try again Later! "+str(e))
                 return render(request,'login/signup.html')
         else:
             return render(request,'login/signup.html')
-    else:
-        form=userprofileform()
-        args= {'form':form}
     return render(request,'login/signup.html',args)
