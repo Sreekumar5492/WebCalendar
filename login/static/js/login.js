9b1100bb14eee6c53287a2830ea85aab717b7014
sreekumar <sreekumarvpl@gmail.com>
2016-02-12 10:15:40 +0530

    nospace in validation

--- login/static/js/login.js
+++ login/static/js/login.js
@@ -3,75 +3,84 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
-$('#signupform').validate({
-    errorClass: "invalid",
-    validClass: "valid",
-    rules: {
-        name: {
-            required: true,
-            minlength: 2
-        },
-        email: {
-            required: true,
-            email: true
-        },
-        username: {
-            required: true,
-            minlength: 3,
-            remote: {
-                url: "/user_available/",
-                type: "post",
-                data: {
-                    username: function () {
-                        return $("#username").val();
+$(document).ready(function () {
+    jQuery.validator.addMethod("noSpace", function (value, element) {
+        return value.indexOf(" ") < 0 && value != "";
+    }, "No space please and don't leave it empty");
+    $('#signupform').validate({
+        errorClass: "invalid",
+        validClass: "valid",
+        rules: {
+            name: {
+                required: true,
+                minlength: 2,
+                noSpace:true
+            },
+            email: {
+                required: true,
+                email: true,
+                noSpace:true
+            },
+            username: {
+                required: true,
+                noSpace:true,
+                minlength: 3,
+                remote: {
+                    url: "/user_available/",
+                    type: "post",
+                    data: {
+                        username: function () {
+                            return $("#username").val();
+                        }
                     }
                 }
+            },
+            password1: {
+                required: true,
+                noSpace:true,
+                minlength: 4
+            },
+            password2: {
+                required: true,
+                noSpace:true,
+                minlength: 4,
+                equalTo: '#password1'
+            }
+
+        },
+        errorElement: 'div',
+        errorPlacement: function (error, element) {
+            var placement = $(element).data('error');
+            if (placement) {
+                $(placement).append(error);
+            } else {
+                error.insertAfter(element);
+            }
+
+        }
+    });
+    $('#loginform').validate({
+        errorClass: "invalid",
+        validClass: "valid",
+        rules: {
+            username: {
+                required: true,
+                minlength: 3
+            },
+            password: {
+                required: true,
+                minlength: 3
             }
         },
-        password1: {
-            required: true,
-            minlength: 4
-        },
-        password2: {
-            required: true,
-            minlength: 4,
-            equalTo: '#password1'
+        errorElement: 'div',
+        errorPlacement: function (error, element) {
+            var placement = $(element).data('error');
+            if (placement) {
+                $(placement).append(error);
+            } else {
+                error.insertAfter(element);
+            }
         }
+    });
 
-    },
-    errorElement: 'div',
-    errorPlacement: function (error, element) {
-        var placement = $(element).data('error');
-        if (placement) {
-            $(placement).append(error);
-        } else {
-            error.insertAfter(element);
-        }
-
-    }
-});
-$('#loginform').validate({
-    errorClass: "invalid",
-    validClass: "valid",
-    rules: {
-        username: {
-            required: true,
-            minlength: 3
-        },
-        password: {
-            required: true,
-            minlength: 3
-        }
-    },
-    errorElement: 'div',
-    errorPlacement: function (error, element) {
-        var placement = $(element).data('error');
-        if (placement) {
-            $(placement).append(error);
-        } else {
-            error.insertAfter(element);
-        }
-    }
-});
-
+})
