7e80353b9e21520b5a1278b570699d14c634264e
sreekumar <sreekumarvpl@gmail.com>
2016-02-15 01:32:37 +0530

    logo added

--- web_calendar/static/js/home.js
+++ web_calendar/static/js/home.js
@@ -11,6 +11,22 @@ $(document).ready(function() {
 
     $('#calendar').fullCalendar({
         // put yheight:465our options and callbacks here
+        events: [
+        {
+            title  : 'event1',
+            start  : '2016-02-01'
+        },
+        {
+            title  : 'event2',
+            start  : '2016-02-05',
+            end    : '2016-02-07'
+        },
+        {
+            title  : 'event3',
+            start  : '2016-02-09T12:30:00',
+            allDay : false // will make the time show
+        }
+    ]
     });
 
 });
